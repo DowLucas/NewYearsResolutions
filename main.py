c86b6bf85aef54c869eff3154b7e56b161561e0b
frappochino <lucdow7@gmail.com>
2020-01-01 15:34:46 +0100

    first

---
+++ main.py
@@ -0,0 +1,117 @@
+import argparse
+import json
+from datetime import datetime
+import re
+
+class Resolutions:
+    dir = 'JSON'
+    name = 'main.json'
+    full_dir = dir+'/'+name
+    def __init__(self):
+        pass
+
+    def reset(self):
+        confirm = input('Please Confirm file reset by typing "confirm": ')
+        if confirm == 'confirm':
+            with open(self.full_dir, 'w') as j:
+                structure = {
+                    'Entries': [
+
+                    ]
+                }
+                json.dump(structure, j, indent=4)
+        print('Reset complete...')
+        quit()
+
+
+    @staticmethod
+    def getDate():
+        now = datetime.now()
+        return now.strftime("%d-%m-%Y")
+
+    def getEntry(self, date):
+        with open(self.full_dir) as j:
+            data = json.load(j)
+            entries = data['Entries'][0]
+            return entries[date]
+
+
+    def addEntry(self):
+        now = datetime.now()
+        Date = now.strftime("%d-%m-%Y")
+
+        with open(self.full_dir) as j:
+            data = json.load(j)
+            try:
+                if data["Entries"][0][Date]:
+                    raise Exception("Entry for today already exist!\n")
+            except:
+                pass
+            structure = {
+                Date: {
+                    'Push-ups': input('Number of push-ups: '),
+                    'Sit-ups': input('Number of sit-ups: '),
+                    'Went to Gym': input('Went to gym?: '),
+                }
+            }
+
+            data['Entries'].append(structure)
+            j.close()
+
+        with open(self.full_dir, 'w') as j:
+            json.dump(data, j, indent=4)
+
+
+
+    def updateEntry(self):
+        date = self.getDate()
+        print(f'----- Update Entry -----\nCurrent Date: {date}')
+        input_date = input('Please input date you wish to enter (format: dd-mm-yyyy) type "t" for today: ')
+        if input_date == 't':
+            entry = self.getEntry(date)
+        else:
+            try:
+                datetime.strptime(date, "%d-%m-%Y")
+            except Exception as E:
+                print(E)
+            date = input_date
+            entry = self.getEntry(date)
+
+        print('\nUpdate values of entry. Insert desired value or press enter to be unchanged.\n')
+
+        for key in entry.keys():
+            inp = input(key+f' (current: {entry[key]}): ')
+            if inp == '':
+                inp = entry[key]
+            entry[key] = inp
+
+
+        with open(self.full_dir) as j:
+            data = json.load(j)
+            data['Entries'][0][date] = entry
+            with open(self.full_dir, 'w') as j:
+                json.dump(data, j, indent=4)
+
+
+
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Add/Update Entries for each day')
+    parser.add_argument('--add', const='add', help='Add entry', action='store_const')
+    parser.add_argument('--update', const='update', help='Update entry', action='store_const')
+    parser.add_argument('--reset', const='reset', help='Update entry', action='store_const')
+
+    args = parser.parse_args()
+
+    res = Resolutions()
+
+    if args.add:
+        res.addEntry()
+    elif args.update:
+        res.updateEntry()
+    elif args.reset:
+        res.reset()
+
+
+
